5ce207440ee170a9e19a18914525d04e084a9753
Joost <113507203+JoostKraan@users.noreply.github.com>
2024-06-08 23:36:27 +0200

    Base Website
    Placeholders Currently

---
+++ Portfolio/Javascript.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const carouselContainer = document.querySelector('.carousel-container');
+    const images = document.querySelectorAll('.carousel img');
+    let index = 0;
+  
+    function showNextImage() {
+      index++;
+      if (index >= images.length) {
+        index = 0;
+      }
+      const offset = -index * 100;
+      carouselContainer.style.transform = `translateX(${offset}%)`;
+    }
+  
+    setInterval(showNextImage, 3000);
+  });
