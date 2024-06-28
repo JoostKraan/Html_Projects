ddb484100b6108d90a9ee0e6dcd536ac5dd8933f
Joost <113507203+JoostKraan@users.noreply.github.com>
2024-06-28 15:32:17 +0200

    somewhat showable

--- Portfolio/Javascript.js
+++ Portfolio/Javascript.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const carouselContainer = document.querySelector('.carousel-container');
-    const images = document.querySelectorAll('.carousel img');
-    let index = 0;
-  
-    function showNextImage() {
-      index++;
-      if (index >= images.length) {
-        index = 0;
-      }
-      const offset = -index * 100;
-      carouselContainer.style.transform = `translateX(${offset}%)`;
+  const carouselContainer = document.querySelector('.carousel-container');
+  const images = document.querySelectorAll('.carousel img');
+  let index = 0;
+
+  function showNextImage() {
+    index++;
+    if (index >= images.length) {
+      index = 0;
     }
-  
-    setInterval(showNextImage, 3000);
-  });
+    const offset = -index * 100;
+    carouselContainer.style.transform = `translateX(${offset}%)`;
+  }
+
+  setInterval(showNextImage, 3000);
+});
